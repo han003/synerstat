1217038d80571edd5aa75024d19bea97b59657c3
Joar <jagolegion@gmail.com>
2022-04-30 00:38:18 +0200

    temp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,31 +7,34 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {FormsModule} from '@angular/forms';
-import {FlexModule} from '@angular/flex-layout';
+import {FlexModule, GridModule} from '@angular/flex-layout';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatTableModule} from '@angular/material/table';
 import {MatIconModule} from '@angular/material/icon';
 import { NumericPipe } from './pipes/numeric.pipe';
+import {ClipboardModule} from '@angular/cdk/clipboard';
 
 @NgModule({
   declarations: [
     AppComponent,
     NumericPipe
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        MatFormFieldModule,
-        MatInputModule,
-        FormsModule,
-        FlexModule,
-        MatButtonModule,
-        MatCardModule,
-        MatTableModule,
-        MatIconModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    FlexModule,
+    MatButtonModule,
+    MatCardModule,
+    MatTableModule,
+    MatIconModule,
+    ClipboardModule,
+    GridModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
